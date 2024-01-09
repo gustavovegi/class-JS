f6daa85242e34c732d09e7791a34dc69f18f3f8c
Gustavo Vegi <gustavovegi15@gmail.com>
2024-01-09 12:08:35 -0300

    type string

--- JS VSCODE/types.js
+++ JS VSCODE/types.js
@@ -1,3 +1,32 @@
 //number
 
 const numero = 3;
+const numero2 = 2;
+
+const soma = numero + numero2;
+const multiplicação = numero * numero2;
+
+console.log(soma)
+console.log(multiplicação)
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuantesemZero = .5;
+
+const novaOperação = numero / numeroPontoFlutuante;
+
+console.log(novaOperação)
+
+//NaN -> não é um numero 
+
+const alura = "alura";
+console.log (alura * numero)
+
+const texto1="Olá, mundo";
+const texto2='Olá. mundo';
+const senha="senhaSupersegura456!"
+const stringdeNumeros="34567"
+
+const citação = 'o Leo disse "oi"';
+console.log(citação)
