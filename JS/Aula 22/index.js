2b300baaafe4f66bd349a3b8ede376cb45d4e984
Gustavo Vegi <gustavovegi15@gmail.com>
2024-03-20 18:53:58 -0300

    Aula 21-23

---
+++ JS/Aula 22/index.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome:'Miranda',
+    idade:30,
+    endereco: {
+        rua: 'Av Brasil',
+        numero: 320
+    }
+};
+
+const {endereco:{rua, numero}} = pessoa;
+console.log(rua, numero);
