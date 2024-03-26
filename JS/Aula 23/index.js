0061de45eba3ed380f066c275a5fcb0b76775f7e
Gustavo Vegi <gustavovegi15@gmail.com>
2024-03-26 17:32:52 -0300

    Aula 23

---
+++ JS/Aula 23/index.js
@@ -0,0 +1,5 @@
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Gustavo', 'Vegi', 'Ribeiro', 'AB']
+
+for (let i =0; i < frutas.length; i++) {
+    console.log(`índice ${i}`, frutas[i]);
+}
---
+++ JS/Aula 24/js/main.js
@@ -0,0 +1,8 @@
+const elementos = [
+    {tag: 'p', texto: 'Qualquer texto'},    
+    {tag: 'div', texto: 'Qualquer texto 2'},    
+    {tag: 'section', texto: 'Qualquer texto 3'},    
+    {tag: 'footer', texto: 'Qualquer texto 4'}    
+];
+
+console.log(elementos);
